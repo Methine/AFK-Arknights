45129b1b10aefe2c581323f21c13652c20ab73b7
carbyne <chenhao_1998@163.com>
2020-02-06 21:52:14 +0800

    Send QQMsg to Phone when run out of lizhi

--- mrfz.py
+++ mrfz.py
@@ -4,8 +4,9 @@
 #This scripts is also suitable for Emulator, you need to modify some of the code, DON'T forget to switch on adb mode.
 #If you need to add other levels, please add it to dict according to the comment.
 #IMPORTANT, EVERYTIME you need to edit input part.
-#2020/2/5 Add photo contrast to avoid bug caused by network jam
+#2020/2/5 Add photo contrast to avoid bug caused by network jam.
 #2020/2/6 Remove screenshot saved on sdcard.
+#2020/2/6 Send QQMsg to Phone when run out of lizhi
 #Thanks for Boar_SJTU for his prototype.
 
 import os, time
@@ -13,7 +14,28 @@ import random
 from skimage.measure import compare_ssim
 from imageio import imread
 import numpy as np
+import win32gui
+import win32con
+import win32clipboard as w
 
+class sendMsg():
+	def __init__(self,receiver,msg):
+		self.receiver=receiver
+		self.msg=msg
+		self.setText()
+	#设置剪贴版内容
+	def setText(self):
+		w.OpenClipboard()
+		w.EmptyClipboard()
+		w.SetClipboardData(win32con.CF_UNICODETEXT, self.msg)
+		w.CloseClipboard()
+	#发送消息
+	def sendmsg(self):
+		qq=win32gui.FindWindow(None,self.receiver)
+		win32gui.SendMessage(qq,win32con.WM_PASTE , 0, 0) #win32on 见site-packages\win32\lib\win32con.py,有的博文里出现的770对用的就是win32con.WM_PASTE
+		win32gui.SendMessage(qq, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+	
+	
 def imgcontrast():
 	img1 = imread('org.png')
 	os.popen('adb shell screencap -p /sdcard/c.png')
@@ -25,7 +47,15 @@ def imgcontrast():
 	return float(ssim)
 
 #dict[level: [time(s), cost]]
-dict = {'4-4': [3 * 60 + 40, 18], '1-7': [1 * 60 + 25, 6], 'CE-5': [2 * 60 + 15, 30], 'PR-D-1': [2 * 60, 18], 'PR-C-1': [1 * 60 + 37, 18], 'S3-1': [1 * 60 + 35, 15], 'S3-4': [1 * 60 + 50, 15], '4-7': [2 * 60 + 15, 18]}
+dict = {}
+dict['1-7'] = [1 * 60 + 25, 6]
+dict['4-4'] = [3 * 60 + 40, 18]
+dict['4-7'] = [2 * 60 + 15, 18]
+dict['CE-5'] = [2 * 60 + 15, 30]
+dict['PR-C-1'] = [1 * 60 + 37, 18]
+dict['PR-D-1'] = [2 * 60, 18]
+dict['S3-1'] = [1 * 60 + 35, 15]
+dict['S3-4'] = [1 * 60 + 50, 15]
 
 #adb test
 #If adb says no devices, modify and add the code below(Bluestacks)
@@ -33,7 +63,7 @@ dict = {'4-4': [3 * 60 + 40, 18], '1-7': [1 * 60 + 25, 6], 'CE-5': [2 * 60 + 15,
 os.system('adb devices')
 
 #input part, modify it before use.
-lizhi = 75
+lizhi = 1
 level = 'CE-5'
 
 #caculation
@@ -82,5 +112,11 @@ for i in range(turn):
 	os.system('adb shell input tap 1683 755')
 	print str(i + 1) + '/' + str(turn) + ' Done'
 	time.sleep(5 + random.randint(0,3))
+
+#Send QQ Msg to Phone when run out of lizhi
+receiver = '我的Android手机'.decode('utf8').encode('gbk', 'ignore')
+msg = '理智已经耗尽'.decode('utf8')
+qq = sendMsg(receiver,msg)
+qq.sendmsg()
 
 #os.system('adb -d shell sh /data/data/me.piebridge.brevent/brevent.sh')
