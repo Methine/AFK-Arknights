7331f7a4bfc54a59673487dd6d678e14e9d72cd1
carbyne <chenhao_1998@163.com>
2020-02-15 15:41:07 +0800

    Remove all impractical functions.

---
+++ mrfz_simple.py
@@ -0,0 +1,64 @@
+# -*- coding:utf-8 -*-
+#AFK scripts for Arknights
+#You may need adb kits. Download it from Android Official Website. (Remember, add it to PATH, otherwise you need to put this scripts in the same folder with adb.exe)
+#This scripts is also suitable for Emulator, you need to modify some of the code, DON'T forget to switch on adb mode.
+#If you need to add other levels, please add it to dict according to the comment.
+#IMPORTANT, EVERYTIME you need to edit input part.
+#Thanks for Boar_SJTU for his prototype.
+#Remove all impractical function.
+
+import os, time
+import random
+
+#input part, modify it before use.
+lizhi = 19
+level = '1-7'
+
+#dict[level: [time(s), cost]]
+dict = {}
+dict['1-7'] = [1 * 60 + 25, 6]
+dict['4-4'] = [2 * 60 + 35, 18]
+dict['4-7'] = [2 * 60 + 15, 18]
+dict['4-8'] = [2 * 60 + 10, 21]
+dict['4-9'] = [2 * 60 + 40, 21]
+dict['CE-5'] = [2 * 60 + 15, 30]
+dict['PR-C-1'] = [1 * 60 + 37, 18]
+dict['PR-D-1'] = [2 * 60, 18]
+dict['S3-1'] = [1 * 60 + 35, 15]
+dict['S3-4'] = [1 * 60 + 50, 15]
+
+#adb test
+#If adb says no devices, modify and add the code below(Bluestacks)
+#os.system('adb connect 127.0.0.1:5555')
+os.system('adb devices')
+
+#caculation
+time_cost = dict[level][0]
+cost = dict[level][1]
+turn = lizhi // cost 
+print 'level: ' + level + ', ' + str(turn) + ' turn(s) in all'
+
+#loop part 
+'''
+#compatible for 1920x1080 resolution
+for i in range(turn):
+	os.system('adb shell input tap 1752 1011')
+	time.sleep(5 + random.randint(0,3))
+	os.system('adb shell input tap 1683 755')
+	time.sleep(time_cost + random.randint(0,3))
+	os.system('adb shell input tap 1683 755')
+	print str(i + 1) + '/' + str(turn) + ' Done'
+	time.sleep(5 + random.randint(0,3))
+'''
+
+
+#compatible for 2340x1080 resolution
+for i in range(turn):
+	os.system('adb shell input tap 2169 997')
+	time.sleep(5 + random.randint(0,3))
+	os.system('adb shell input tap 1872 741')
+	time.sleep(time_cost + random.randint(0,3))
+	os.system('adb shell input tap 1683 755')
+	print str(i + 1) + '/' + str(turn) + ' Done'
+	time.sleep(5 + random.randint(0,3))
+
